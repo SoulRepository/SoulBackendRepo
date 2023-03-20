64213aea998838833f9291cde0fb349a3816dad9
Nazar Rohozhuk <nazarrrka@gmail.com>
2023-03-20 11:46:14 +0200

    fix: enable cors

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: true,
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
   });
   app.setGlobalPrefix('api');
   const configService = app.get<ConfigService<ConfigSchema>>(ConfigService);
